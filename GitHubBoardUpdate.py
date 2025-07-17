0eb3cb4edb8677545c1adc9bc8894a8dc00e64dc
LBecker <beckerleon250@gmail.com>
2025-07-17 16:07:42 +0200

    Remove duplicate job

--- GitHubBoardUpdate.py
+++ GitHubBoardUpdate.py
@@ -27,14 +27,7 @@ def setup_github_board_update(bot: Bot, scheduler: AsyncIOScheduler):
     # Round to next hour
     next_hour = get_next_interval()
 
-    # Schedule first run at the top of the next update_interval_minutes relative to the current hour
-    scheduler.add_job(
-        run_periodic_update,
-        trigger=DateTrigger(run_date=next_hour)
-    )
-    logger.log(msg=f"Next GitHub Board update scheduled for {next_hour} UTC", level=logging.INFO)
-
-    # Schedule recurring run every update_interval_minutes minutes after that
+    # Schedule recurring run every update_interval_minutes minutes after next fitting interval
     scheduler.add_job(
         run_periodic_update,
         trigger=IntervalTrigger(minutes=update_interval_minutes, start_date=next_hour)
